d2e40a95a723da7b53d661442bf368bf0264ed68
Saurav Agarwal <SauravAg@seas.upenn.edu>
2022-11-25 20:57:19 -0500

    Update installation

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # CoverageControl
+Provides environment for Coverage Control problem.
+
+## Requirements
+    - CUDA nvcc
+    - Eigen
+    - Boost
+
+## Installation
+
+Copy the `setup.sh` file. It will clone the repository and install. 
+Assuming main workspace directory: ${HOME}/CoverageControl_ws. Change `setup.sh` otherwise.
+
+```
+bash setup.sh
+```
+Add the following lines to your `.bashrc` file and then `source ~/.bashrc` so that Python can find the libraries:
+```
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${HOME}/CoverageControl_ws/install/lib"
+export PYTHONPATH="${PYTHONPATH}:${HOME}/CoverageControl_ws/install/lib"
+```
+
+Go to `CoverageControl/scripts/python` and check if the file `coverage.py` works. It contains minimal examples.
--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+WORKSPACE_DIR=${HOME}/CoverageControl_ws
+BUILD_DIR=${WORKSPACE_DIR}/build
+INSTALL_DIR=${WORKSPACE_DIR}/install
 
-COVERAGE_CONTROL_INSTALL_PATH=${HOME}/CoverageControl/
-mkdir -p build
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX=${COVERAGE_CONTROL_INSTALL_PATH}
-cmake --build build/
-cmake --install build/
+mkdir -p ${WORKSPACE_DIR}/src
+cd ${WORKSPACE_DIR}/src
+git clone --recursive git@github.com:AgarwalSaurav/CoverageControl.git
+cd ..
+
+cmake -S ${WORKSPACE_DIR}/src/CoverageControl/extern/pybind11 -B ${BUILD_DIR}/pybind11 -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
+cmake --install ${BUILD_DIR}/pybind11
+
+cmake -S ${WORKSPACE_DIR}/src/CoverageControl -B ${BUILD_DIR}/CoverageControl -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
+cmake --build ${BUILD_DIR}/CoverageControl
+cmake --install ${BUILD_DIR}/CoverageControl
