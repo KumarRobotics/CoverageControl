ec0f9537031c9cda267dbae24b1d9a1f78240d53
Saurav Agarwal <SauravAg@seas.upenn.edu>
2022-12-01 20:52:39 -0500

    Fix eigen auto issue

--- include/CoverageControl/cgal_config.h
+++ include/CoverageControl/cgal_config.h
@@ -8,6 +8,7 @@
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 // includes for defining the Voronoi diagram adaptor
 #include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/Arr_walk_along_line_point_location.h>
 
 // typedefs for defining the adaptor
 /* typedef CGAL::Exact_predicates_inexact_constructions_kernel                  K; */
@@ -36,6 +37,7 @@ typedef CGAL::Polygon_with_holes_2<K> Polygon_with_holes_2;
 
 typedef CGAL::Arr_linear_traits_2<K> Traits_2;
 typedef CGAL::Arrangement_2<Traits_2> Arrangement_2;
+typedef CGAL::Arr_walk_along_line_point_location<Arrangement_2> CGAL_pl;
 
 /* typedef K::Intersect_2 Intersect_2; */
 /* typedef K::Plane_3 Plane_3; */
--- src/voronoi.cpp
+++ src/voronoi.cpp
@@ -7,78 +7,89 @@
 namespace CoverageControl {
 
 	void Voronoi::ComputeMassCentroid(VoronoiCell &vcell) {
-		/* std::cout << "Entering ComputeMassCentroid " << std::endl; */
-		vcell.mass = 0;
-		vcell.obj = 0;
-		vcell.centroid = Point2(0,0);
-		Polygon_2 cgal_poly;
-		for(auto const &p:vcell.cell) {
-			cgal_poly.push_back(CGAL_Point2(p.x(), p.y()));
-		}
+		vcell.mass = 0; vcell.obj = 0;
+		vcell.centroid = Point2{0,0};
+		auto &cell = vcell.cell;
+		int n = cell.size();
+		/* Polygon_2 cgal_poly; */
+		/* for(auto const &p:vcell.cell) { */
+		/* 	cgal_poly.push_back(CGAL_Point2(p.x(), p.y())); */
+		/* } */
 
-		auto start_vertex_itr = cgal_poly.vertices_circulator();
-		auto curr_vertex_itr = start_vertex_itr;
-		std::advance(curr_vertex_itr, 1);
-		auto left_vertex_itr = start_vertex_itr;
-		while(curr_vertex_itr != start_vertex_itr) {
-			if(curr_vertex_itr->x() < left_vertex_itr->x()) {
-				left_vertex_itr = curr_vertex_itr;
+		int left_id = 0;
+		int right_id = 0;
+		for(int i = 1; i < n; ++i) {
+			if(cell[i].x() < cell[left_id].x()) {
+				left_id = i;
 			}
-			std::advance(curr_vertex_itr, 1);
+			if(cell[i].x() > cell[right_id].x()) {
+				right_id = i;
+			}
 		}
 
 		/* std::cout << "Found left vertex" << std::endl; */
-		int min_i = std::round(CGAL::to_double(left_vertex_itr->x())/resolution_);
+		int min_i = std::round(cell[left_id].x()/resolution_);
 		min_i = min_i < 0 ? 0 : min_i;
 
-		auto right_vertex_itr = cgal_poly.right_vertex();
-		int max_i = std::round(CGAL::to_double(right_vertex_itr->x())/resolution_);
+		int max_i = std::round(cell[right_id].x()/resolution_);
 		max_i = max_i > map_size_ ? map_size_ : max_i;
 
-		auto cc_pt_itr = std::next(left_vertex_itr); // Counter-clockwise pointer
-		auto c_pt_itr = std::prev(left_vertex_itr); // Clockwise pointer
+		auto next_id = [=](int const id) { return (id + 1)%n; };
+		auto prev_id = [=](int const id) { return id == 0 ? (n - 1) : (id - 1); };
+
+		auto cc_pt_id = next_id(left_id); // Counter-clockwise pointer
+		auto c_pt_id  = prev_id(left_id); // Clockwise pointer
 
 		/* std::cout << "cc pointers set" << std::endl; */
 		for(int i = min_i; i < max_i; ++i) {
 			double x = i * resolution_ + resolution_/2.;
 			while(true) {
-				if(cc_pt_itr == left_vertex_itr) {break;}
-				if(CGAL::to_double(cc_pt_itr->x()) < x) { std::advance(cc_pt_itr, 1); }
+				if(cc_pt_id == left_id) {break;}
+				if(cell[cc_pt_id].x() < x) { cc_pt_id = next_id(cc_pt_id); }
 				else {break;}
 			}
 			while(true) {
-				if(c_pt_itr == left_vertex_itr) {break;}
-				if(CGAL::to_double(c_pt_itr->x()) < x) { std::advance(c_pt_itr, -1); }
+				if(c_pt_id == left_id) {break;}
+				if(cell[c_pt_id].x() < x) { c_pt_id = prev_id(c_pt_id); }
 				else {break;}
 			}
-			if(CGAL::to_double(c_pt_itr->x()) < x) { break; }
-			if(CGAL::to_double(cc_pt_itr->x()) < x) { break; }
 
-			auto x1 = std::prev(cc_pt_itr)->x();
-			auto y1 = std::prev(cc_pt_itr)->y();
-			auto x2 = cc_pt_itr->x();
-			auto y2 = cc_pt_itr->y();
+			if(cell[cc_pt_id].x() < x) { break; }
+			if(cell[c_pt_id].x() < x) { break; }
+
+			auto prev_pt = cell[prev_id(cc_pt_id)];
+			auto cc_pt = cell[cc_pt_id];
+			auto x1 = prev_pt.x();
+			auto y1 = prev_pt.y();
+			auto x2 = cc_pt.x();
+			auto y2 = cc_pt.y();
+
 			if((x2 - x1) < kEps) { throw std::runtime_error{"Unexpected error!"}; }
 			auto y_lower = y1 + (x - x1) * (y2 - y1) / (x2 - x1);
-			x1 = std::next(c_pt_itr)->x();
-			y1 = std::next(c_pt_itr)->y();
-			x2 = c_pt_itr->x();
-			y2 = c_pt_itr->y();
+
+			auto next_pt = cell[next_id(c_pt_id)];
+			auto c_pt = cell[c_pt_id];
+			x1 = next_pt.x();
+			y1 = next_pt.y();
+			x2 = c_pt.x();
+			y2 = c_pt.y();
+
 			if((x2 - x1) < kEps) { throw std::runtime_error{"Unexpected error!"}; }
 			auto y_upper = y1 + (x - x1) * (y2 - y1) / (x2 - x1);
 
-			int min_j = std::round(CGAL::to_double(y_lower)/resolution_);
+			int min_j = std::round(y_lower/resolution_);
 			min_j = min_j < 0 ? 0 : min_j;
 
-			int max_j = std::round(CGAL::to_double(y_upper)/resolution_);
+			int max_j = std::round(y_upper/resolution_);
 			max_j = max_j > map_size_ ? map_size_ : max_j;
 
 			for(int j = min_j; j < max_j; ++j) {
 				double y = j * resolution_ + resolution_/2.;
+				Point2 curr_pt(x, y);
 				auto map_val = map_->operator()(i, j);
 				vcell.mass += map_val;
-				vcell.centroid = vcell.centroid + Point2(x, y) * map_val;
-				vcell.obj += Point2(x - vcell.site.x(), y - vcell.site.y()).squaredNorm() * map_val;
+				vcell.centroid += curr_pt * map_val;
+				vcell.obj += (curr_pt - vcell.site).squaredNorm() * map_val;
 			}
 		}
 		/* std::cout << "Computed vcell" << std::endl; */
@@ -147,6 +158,7 @@ namespace CoverageControl {
 			return;
 		}
 
+		/* std::cout << "d2 begin" << std::endl; */
 		Delaunay_triangulation_2 dt2;
 		std::vector<CGAL_Point2> CGAL_sites;
 		CGAL_sites.reserve(num_sites_);
@@ -156,81 +168,75 @@ namespace CoverageControl {
 		}
 		dt2.insert(CGAL_sites.begin(), CGAL_sites.end());
 
-		CGAL_Cropped_voronoi_from_delaunay vor;
+		CGAL_DelaunayHelper vor;
 		dt2.draw_dual(vor);
+		/* std::cout << "d2 end" << std::endl; */
 
-		auto s1 = Segment_2(CGAL_Point2(0,0), CGAL_Point2(map_size_,0));
-		vor.segments_.push_back(s1);
-		auto s2 = Segment_2(CGAL_Point2(map_size_,0), CGAL_Point2(map_size_, map_size_));
-		vor.segments_.push_back(s2);
-		auto s3 = Segment_2(CGAL_Point2(map_size_, map_size_), CGAL_Point2(0, map_size_));
-		vor.segments_.push_back(s3);
-		auto s4 = Segment_2(CGAL_Point2(0, map_size_), CGAL_Point2(0, 0));
-		vor.segments_.push_back(s4);
+		vor.segments_.push_back(Segment_2(CGAL_Point2(0,0), CGAL_Point2(map_size_,0)));
+		vor.segments_.push_back(Segment_2(CGAL_Point2(map_size_,0), CGAL_Point2(map_size_, map_size_)));
+		vor.segments_.push_back(Segment_2(CGAL_Point2(map_size_, map_size_), CGAL_Point2(0, map_size_)));
+		vor.segments_.push_back(Segment_2(CGAL_Point2(0, map_size_), CGAL_Point2(0, 0)));
 
-		/* std::cout << "Computing arrangement" << std::endl; */
 		Arrangement_2 arr;
 		CGAL::insert(arr, vor.rays_.begin(), vor.rays_.end());
 		CGAL::insert(arr, vor.lines_.begin(), vor.lines_.end());
 		CGAL::insert(arr, vor.segments_.begin(), vor.segments_.end());
-		/* std::cout << "Arrangement created" << std::endl; */
+		CGAL_pl cgal_pl(arr);
+		/* std::cout << "arr end" << std::endl; */
+
+		if(compute_single_ == true) {
+			auto pt = CGAL_sites[robot_id_];
+			Polygon_2 polygon;
+			auto obj = cgal_pl.locate(pt);
+			auto f = boost::get<Arrangement_2::Face_const_handle>(&obj);
+			CGAL_CCBTraversal<Arrangement_2> ((*f)->outer_ccb(), polygon);
+			if(not polygon.is_counterclockwise_oriented()) {
+				polygon.reverse_orientation();
+			}
+			PointVector poly_points;
+			for(auto const &p:(polygon)) {
+				poly_points.push_back(CGALtoCC(p));
+			}
+			voronoi_cell_.site = CGALtoCC(pt);
+			voronoi_cell_.cell = poly_points;
+
+			ComputeMassCentroid(voronoi_cell_);
+			return;
+		}
 
 		std::list <Polygon_2> polygon_list;
 		CGAL_GeneratePolygons(arr, polygon_list);
-		/* std::cout << "Polygons generated" << std::endl; */
 
-		if(compute_single_ == true) {
-			/* std::cout << " Inside if" << std::endl; */
-			auto const &pt = CGAL_sites[robot_id_];
-			/* std::cout << pt << std::endl; */
-			/* std::cout << "List size: " << polygon_list.size() << std::endl; */
-			for(auto it = polygon_list.begin(); it != polygon_list.end(); ++it) {
-				if(IsPointInPoly(pt, *it) == true) {
-					PointVector poly_points;
-					poly_points.reserve(it->size());
-					for(auto const &p:(*it)) {
-						poly_points.push_back(CGALtoCC(p));
-					}
-					voronoi_cell_.site = CGALtoCC(pt);
-					voronoi_cell_.cell = poly_points;
-					break;
-				}
-			}
-			/* std::cout << "Computing mass" << std::endl; */
-			ComputeMassCentroid(voronoi_cell_);
-			std::cout << "Mass: " << voronoi_cell_.mass << std::endl;
-			/* std::cout << "If ends" << std::endl; */
-			return;
-		}
 		PrunePolygons(polygon_list, map_size_);
 		// Create voronoi_cells_ such that the correct cell is assigned to the robot
+		/* #pragma omp parallel for */
 		for(int iSite = 0; iSite < num_sites_; ++iSite) {
-			auto const &pt = CGAL_sites[iSite];
-			for(auto it = polygon_list.begin(); it != polygon_list.end(); ++it) {
-				if(IsPointInPoly(pt, *it) == true) {
-					PointVector poly_points;
-					poly_points.reserve(it->size());
-					for(auto const &p:(*it)) {
-						poly_points.push_back(CGALtoCC(p));
-					}
-					VoronoiCell vcell;
-					vcell.site = CGALtoCC(pt);
-					vcell.cell = poly_points;
-					voronoi_cells_[iSite] = vcell;
-					it = polygon_list.erase(it);
-					break;
-				}
+			auto pt = CGAL_sites[iSite];
+			Polygon_2 polygon;
+			auto obj = cgal_pl.locate(pt);
+			auto f = boost::get<Arrangement_2::Face_const_handle>(&obj);
+			CGAL_CCBTraversal<Arrangement_2> ((*f)->outer_ccb(), polygon);
+			if(not polygon.is_counterclockwise_oriented()) {
+				polygon.reverse_orientation();
 			}
-		}
-		/* std::cout << "Voronoi Polygon generated: " << voronoi_cells_.size() << std::endl; */
-
-		// Compute mass and centroid of the cells
-		#pragma omp parallel for
-		for(int iCell = 0; iCell < num_sites_; ++iCell) {
-			auto &vcell = voronoi_cells_[iCell];
+			PointVector poly_points;
+			for(auto const &p:(polygon)) {
+				poly_points.push_back(CGALtoCC(p));
+			}
+			VoronoiCell vcell;
+			vcell.site = CGALtoCC(pt);
+			vcell.cell = poly_points;
 			ComputeMassCentroid(vcell);
-			/* ComputeMassCentroid2(vcell); */
+			voronoi_cells_[iSite] = vcell;
 		}
 	}
+	/* std::cout << "Voronoi Polygon generated: " << voronoi_cells_.size() << std::endl; */
+
+	// Compute mass and centroid of the cells
+	/* #pragma omp parallel for */
+	/* 		for(int iCell = 0; iCell < num_sites_; ++iCell) { */
+	/* 			auto &vcell = voronoi_cells_[iCell]; */
+	/* 			ComputeMassCentroid(vcell); */
+	/* 		} */
 
 } /* namespace CoverageControl */
