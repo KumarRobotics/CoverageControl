a6d57f5949f79e7dbe4d9f1cc344f8cdafdeb2bd
Saurav Agarwal <SauravAg@seas.upenn.edu>
2022-12-05 00:45:46 -0500

    Update data generation

--- test/data_generation.cpp
+++ test/data_generation.cpp
@@ -3,6 +3,8 @@
 #include <random>
 #include <string>
 #include <cstdlib>
+#include <fstream>
+#include <chrono>
 
 #include <CoverageControl/constants.h>
 #include <CoverageControl/parameters.h>
@@ -17,27 +19,66 @@ using namespace CoverageControl;
 
 int main(int argc, char** argv) {
 	Parameters params("/home/saurav/CoverageControl_ws/src/CoverageControl/params/parameters.yaml");
-	CoverageSystem env(params, 100, 20);
+	int num_robots = 20;
+	CoverageSystem env(params, 100, num_robots);
 	/* std::cout << "Env created" << std::endl; */
 	std::string map_filename = "data/oracle_map";
 	std::string gnuplot_script = "src/CoverageControl/scripts/gnuplot/plot_map.gp";
 
 	bool plot_map = false;
-	bool write_data = true	;
-	for(int i = 0; i < 100; ++i) {
-		std::cout << i << std::endl;
-		env.StepDataGenerationLocal(10);
-		auto oracle_map = env.GetOracleMap();
-
-		if(plot_map == true) {
-			std::stringstream ss;
-			ss << std::setw(4) << std::setfill('0') << i;
-			std::string s = ss.str();
-			std::string imap_name = map_filename + s;
-			MapUtils::WriteMap(oracle_map, imap_name + ".dat");
-			std::string gnuplot_command = "gnuplot -c " + gnuplot_script + " " + imap_name + " 1 " + std::to_string(params.pResolution) + " " + std::to_string(params.pWorldMapSize * params.pResolution);
-			std::system(gnuplot_command.c_str());
+	bool write_data = true;
+	auto start = std::chrono::steady_clock::now();
+	for(int iNum = 0; iNum < 1000; ++iNum) {
+		std::string data_filename = "data/cnn_data.ssv";
+		std::ofstream out_file(data_filename);
+		if(!out_file) {
+			throw std::runtime_error {"Cannot open file"};
 		}
+		for(int i = 0; i < 100; ++i) {
+			std::cout << iNum << " " << i << std::endl;
+			env.StepDataGenerationLocal(10);
+			if(write_data) {
+				for(int iRobot = 0; iRobot < num_robots; ++iRobot) {
+					auto robot_local_map = env.GetRobotLocalMap(iRobot);
+					auto comm_map = env.GetCommunicationMap(iRobot);
+					auto voronoi_cell = env.GetVoronoiCell(iRobot);
+					Point2 centroid = voronoi_cell.centroid;
+					double mass = voronoi_cell.mass;
+					for(int ix = 0; ix < params.pLocalMapSize; ++ix) {
+						for(int iy = 0; iy < params.pLocalMapSize; ++iy) {
+							out_file << robot_local_map(ix, iy) << " ";
+						}
+					}
+					for(int ix = 0; ix < params.pLocalMapSize; ++ix) {
+						for(int iy = 0; iy < params.pLocalMapSize; ++iy) {
+							out_file << comm_map(ix, iy) << " ";
+						}
+					}
+					out_file << centroid.x() << " " << centroid.y() << " " << mass << std::endl;
+				}
+			}
+
+			if(plot_map == true) {
+				auto oracle_map = env.GetOracleMap();
+				std::stringstream ss;
+				ss << std::setw(4) << std::setfill('0') << i;
+				std::string s = ss.str();
+				std::string imap_name = map_filename + s;
+				MapUtils::WriteMap(oracle_map, imap_name + ".dat");
+				std::string gnuplot_command = "gnuplot -c " + gnuplot_script + " " + imap_name + " 1 " + std::to_string(params.pResolution) + " " + std::to_string(params.pWorldMapSize * params.pResolution);
+				std::system(gnuplot_command.c_str());
+			}
+		}
+		out_file.close();
+		std::stringstream ss;
+		ss << std::setw(4) << std::setfill('0') << iNum;
+		std::string s = ss.str();
+		std::string tar_command = "tar -Jcvf ";
+		tar_command += "data/cnn_data/cnn_data_" + s +  ".tar.xz data/cnn_data.ssv";
+		std::system(tar_command.c_str());
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 	}
 	return 0;
 }
