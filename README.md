55283d26cdc9d0228e442fcbb1522d087a605226
Saurav Agarwal <SauravAg@seas.upenn.edu>
2022-12-20 00:49:17 -0500

    Update instructions

--- README.md
+++ README.md
@@ -20,36 +20,35 @@ Python packages:
 
 ## Installation
 
-Workspace directory: `${HOME}/CoverageControl_ws`. Change as necessary.
+Workspace directory: `${HOME}/CoverageControl_ws`. Change as necessary.   
+Add the following lines to your `.bashrc` file and then `source ~/.bashrc` or relogin:
 ```bash
-export WORKSPACE_DIR=${HOME}/CoverageControl_ws
-mkdir -p ${WORKSPACE_DIR}/src
-git clone git@github.com:AgarwalSaurav/CoverageControl.git ${WORKSPACE_DIR}/src/CoverageControl
+export COVERAGECONTROL_WS="${HOME}/CoverageControl_ws/"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${COVERAGECONTROL_WS}/install/lib/"
+export PYTHONPATH="${PYTHONPATH}:${COVERAGECONTROL_WS}/install/lib/"
+```
+
+```bash
+mkdir -p ${COVERAGECONTROL_WS}/src
+git clone git@github.com:AgarwalSaurav/CoverageControl.git ${COVERAGECONTROL_WS}/src/CoverageControl
 ```
 
 Install external packages:  
-Inside `${WORKSPACE_DIR}/src/CoverageControl`
+Inside `${COVERAGECONTROL_WS}/src/CoverageControl`
 ```bash
 bash setup.sh -i
 ```
 
-Add the following lines to your `.bashrc` file and then `source ~/.bashrc` or relogin:
-```bash
-export COVERAGECONTROL_PATH="${WORKSPACE_DIR}/install/"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${WORKSPACE_DIR}/install/lib/"
-export PYTHONPATH="${PYTHONPATH}:${WORKSPACE_DIR}/install/lib/"
-```
 
 Install python API:
 ```bash
-export WORKSPACE_DIR=${HOME}/CoverageControl_ws
-pip install ${WORKSPACE_DIR}/src/CoverageControl
+pip install ${COVERAGECONTROL_WS}/src/CoverageControl
 ```
 
 Go to `CoverageControl/scripts/python` and check if the file `coverage.py` works. It contains minimal examples.
 
 ## Update
-Inside `${WORKSPACE_DIR}/src/CoverageControl`
+Inside `${COVERAGECONTROL_WS}/src/CoverageControl`
 
 ```bash
 bash setup.sh -u
--- setup.sh
+++ setup.sh
@@ -12,17 +12,16 @@ while getopts 'ui' flag; do
 esac
 done
 
-WORKSPACE_DIR=${HOME}/CoverageControl_ws
-BUILD_DIR=${WORKSPACE_DIR}/build
-INSTALL_DIR=${WORKSPACE_DIR}/install
+BUILD_DIR=${COVERAGECONTROL_WS}/build
+INSTALL_DIR=${COVERAGECONTROL_WS}/install
 
-mkdir -p ${WORKSPACE_DIR}/src
+mkdir -p ${COVERAGECONTROL_WS}/src
 
 InstallCGAL () {
 	echo "Setting up CGAL"
-	wget https://github.com/CGAL/cgal/releases/download/v5.5.1/CGAL-5.5.1-library.tar.xz -P ${WORKSPACE_DIR}/src
-	tar -xf ${WORKSPACE_DIR}/src/CGAL-5.5.1-library.tar.xz -C ${WORKSPACE_DIR}/src/
-	cmake -S ${WORKSPACE_DIR}/src/CGAL-5.5.1 -B ${BUILD_DIR}/cgal -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
+	wget https://github.com/CGAL/cgal/releases/download/v5.5.1/CGAL-5.5.1-library.tar.xz -P ${COVERAGECONTROL_WS}/src
+	tar -xf ${COVERAGECONTROL_WS}/src/CGAL-5.5.1-library.tar.xz -C ${COVERAGECONTROL_WS}/src/
+	cmake -S ${COVERAGECONTROL_WS}/src/CGAL-5.5.1 -B ${BUILD_DIR}/cgal -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
 	cmake --install ${BUILD_DIR}/cgal
 	if [ $? -eq 0 ]; then
 		echo "cgal install succeeded"
@@ -30,13 +29,15 @@ InstallCGAL () {
 		echo "cgal install failed"
 		exit 1
 	fi
+	rm ${COVERAGECONTROL_WS}/src/CGAL-5.5.1-library.tar.xz
+	rm -rf ${COVERAGECONTROL_WS}/src/CGAL-5.5.1
 }
 
 InstallGeoGraphicLib () {
 	echo "Setting up geographiclib"
-	wget https://github.com/geographiclib/geographiclib/archive/refs/tags/v2.1.2.tar.gz -P ${WORKSPACE_DIR}/src
-	tar -xf ${WORKSPACE_DIR}/src/v2.1.2.tar.gz -C ${WORKSPACE_DIR}/src/
-	cmake -S ${WORKSPACE_DIR}/src/geographiclib-2.1.2 -B ${BUILD_DIR}/geographiclib -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
+	wget https://github.com/geographiclib/geographiclib/archive/refs/tags/v2.1.2.tar.gz -P ${COVERAGECONTROL_WS}/src
+	tar -xf ${COVERAGECONTROL_WS}/src/v2.1.2.tar.gz -C ${COVERAGECONTROL_WS}/src/
+	cmake -S ${COVERAGECONTROL_WS}/src/geographiclib-2.1.2 -B ${BUILD_DIR}/geographiclib -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
 	cmake --build ${BUILD_DIR}/geographiclib
 	cmake --install ${BUILD_DIR}/geographiclib
 	if [ $? -eq 0 ]; then
@@ -45,13 +46,15 @@ InstallGeoGraphicLib () {
 		echo "geographiclib install failed"
 		exit 1
 	fi
+	rm ${COVERAGECONTROL_WS}/src/v2.1.2.tar.gz
+	rm -rf ${COVERAGECONTROL_WS}/src/geographiclib-2.1.2
 }
 
 InstallPybind11 () {
 	echo "Setting up pybind11"
-	wget https://github.com/pybind/pybind11/archive/refs/tags/v2.10.1.tar.gz -P ${WORKSPACE_DIR}/src
-	tar -xf ${WORKSPACE_DIR}/src/v2.10.1.tar.gz -C ${WORKSPACE_DIR}/src/
-	cmake -S ${WORKSPACE_DIR}/src/pybind11-2.10.1 -B ${BUILD_DIR}/pybind11 -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
+	wget https://github.com/pybind/pybind11/archive/refs/tags/v2.10.1.tar.gz -P ${COVERAGECONTROL_WS}/src
+	tar -xf ${COVERAGECONTROL_WS}/src/v2.10.1.tar.gz -C ${COVERAGECONTROL_WS}/src/
+	cmake -S ${COVERAGECONTROL_WS}/src/pybind11-2.10.1 -B ${BUILD_DIR}/pybind11 -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
 	cmake --install ${BUILD_DIR}/pybind11
 	if [ $? -eq 0 ]; then
 		echo "pybind11 install succeeded"
@@ -59,12 +62,14 @@ InstallPybind11 () {
 		echo "pybind11 install failed"
 		exit 1
 	fi
+	rm ${COVERAGECONTROL_WS}/src/v2.10.1.tar.gz
+	rm -rf ${COVERAGECONTROL_WS}/src/pybind11-2.10.1
 }
 
 InstallYamlCPP () {
 	echo "Setting up yaml-cpp"
-	git clone git@github.com:jbeder/yaml-cpp.git ${WORKSPACE_DIR}/src/yaml-cpp
-	cmake -S ${WORKSPACE_DIR}/src/yaml-cpp -B ${BUILD_DIR}/yaml-cpp -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DYAML_BUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release
+	git clone git@github.com:jbeder/yaml-cpp.git ${COVERAGECONTROL_WS}/src/yaml-cpp
+	cmake -S ${COVERAGECONTROL_WS}/src/yaml-cpp -B ${BUILD_DIR}/yaml-cpp -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DYAML_BUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release
 	cmake --build ${BUILD_DIR}/yaml-cpp -j4
 	if [ $? -ne 0 ]; then
 		echo "YAML build failed"
@@ -76,13 +81,14 @@ InstallYamlCPP () {
 		echo "yaml-cpp install failed"
 		exit 1
 	fi
+	rm -rf ${COVERAGECONTROL_WS}/src/yaml-cpp
 }
 
 InstallEigen3 () {
 	echo "Setting up eigen3"
-	wget https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz -P ${WORKSPACE_DIR}/src
-	tar -xf ${WORKSPACE_DIR}/src/eigen-3.4.0.tar.gz -C ${WORKSPACE_DIR}/src/
-	cmake -S ${WORKSPACE_DIR}/src/eigen-3.4.0 -B ${BUILD_DIR}/eigen3 -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
+	wget https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz -P ${COVERAGECONTROL_WS}/src
+	tar -xf ${COVERAGECONTROL_WS}/src/eigen-3.4.0.tar.gz -C ${COVERAGECONTROL_WS}/src/
+	cmake -S ${COVERAGECONTROL_WS}/src/eigen-3.4.0 -B ${BUILD_DIR}/eigen3 -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
 	cmake --install ${BUILD_DIR}/eigen3
 	if [ $? -eq 0 ]; then
 		echo "eigen3 install succeeded"
@@ -90,29 +96,14 @@ InstallEigen3 () {
 		echo "eigen3 install failed"
 		exit 1
 	fi
-}
-
-InstallCoverageControl () {
-	echo "Setting up CoverageControl"
-	git clone git@github.com:AgarwalSaurav/CoverageControl.git ${WORKSPACE_DIR}/src/CoverageControl
-	cmake -S ${WORKSPACE_DIR}/src/CoverageControl -B ${BUILD_DIR}/CoverageControl -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
-	cmake --build ${BUILD_DIR}/CoverageControl
-	if [ $? -ne 0 ]; then
-		echo "CoverageControl build failed"
-	fi
-	cmake --install ${BUILD_DIR}/CoverageControl
-	if [ $? -eq 0 ]; then
-		echo "CoverageControl install succeeded"
-	else
-		echo "CoverageControl install failed"
-		exit 1
-	fi
+	rm ${COVERAGECONTROL_WS}/src/eigen-3.4.0.tar.gz
+	rm -rf ${COVERAGECONTROL_WS}/src/eigen-3.4.0
 }
 
 UpdateCoverageControl () {
 	# Run the following commands to update after a change in the repository
-	# The CoverageControl repository is located in ${WORKSPACE_DIR}/src/CoverageControl
-	cmake -S ${WORKSPACE_DIR}/src/CoverageControl -B ${BUILD_DIR}/CoverageControl -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
+	# The CoverageControl repository is located in ${COVERAGECONTROL_WS}/src/CoverageControl
+	cmake -S ${COVERAGECONTROL_WS}/src/CoverageControl -B ${BUILD_DIR}/CoverageControl -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
 	cmake --build ${BUILD_DIR}/CoverageControl
 	if [ $? -ne 0 ]; then
 		echo "CoverageControl build failed"
@@ -132,6 +123,7 @@ then
 	InstallYamlCPP
 	InstallCGAL
 	InstallGeoGraphicLib
+	UpdateCoverageControl
 fi
 
 if [[ ${UPDATE} ]]
