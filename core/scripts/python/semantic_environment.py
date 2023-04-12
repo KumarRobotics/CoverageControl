718e1080440157544b201fde58c2123e54c644c1
Saurav Agarwal <SauravAg@seas.upenn.edu>
2023-04-12 15:54:01 -0400

    Update semantic environment

--- core/scripts/python/semantic_environment.py
+++ core/scripts/python/semantic_environment.py
@@ -15,6 +15,7 @@ params = pyCoverageControl.Parameters()
 
 world_idf = WorldIDF(params)
 
+# sigma and peak can be randomize, e.g., randomly choose a sigma between params.pMinSigma and params.pMaxSigma
 sigma = params.pMinSigma
 peak = params.pMinPeak
 
@@ -23,10 +24,11 @@ with open(features_filename_csv, newline='') as csvfile:
     for row in csvreader:
         world_idf.AddNormalDistribution(BND(Point2(float(row[0]), float(row[1])), sigma, peak))
 
+# The positions of the robots can be randomized
 robots_positions = PointVector()
 robots_positions.append(Point2(100.0, 100.0))
 robots_positions.append(Point2(50.0, 150.0))
 
 env = CoverageSystem(params, world_idf, robots_positions)
-env.PlotInitMap('./', 'init_map.png')
+env.PlotInitMap('./', 'init_map')
 
