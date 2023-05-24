7cfd12b5cbeb92519de9b24b6fb736ab4abfda9e
Saurav Agarwal <agr.saurav1@gmail.com>
2023-05-24 18:50:04 -0400

    Add jit transformer resize

---
+++ python/tests/data_load.py
@@ -0,0 +1,16 @@
+import sys
+import torch
+import pyCoverageControlTorch as cct
+
+if __name__ == "__main__":
+    # Get filename from argument
+    filename = sys.argv[1]
+    # Load tensor from file
+    tensor_model = torch.jit.load(filename)
+    tensor = list(tensor_model.parameters())[0]
+    # Print tensor
+    print(tensor.dtype)
+    print(tensor.shape)
+    print(tensor.sum())
+
+
---
+++ python/ts_jit/torchvision_resize.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import torch
+import torchvision
+import torchvision.transforms as T
+
+class Resizer(torch.nn.Module):
+    def __init__(self, size):
+        super().__init__()
+        self.size = size
+        self.T = T.Resize(size)
+
+    def forward(self, img):
+        return self.T(img)
+
+if __name__ == "__main__":
+    # Get size from args
+    map_size = int(sys.argv[1])
+    file_name = str(sys.argv[2])
+    scripted_resizer = torch.jit.script(Resizer([map_size,]))
+    scripted_resizer.save(file_name)
