78e9a9dada8eb7391dcb96b9c91814c0da59dd0f
Saurav Agarwal <SauravAg@seas.upenn.edu>
2023-10-02 00:34:24 -0400

    cleanup

--- setup.sh
+++ setup.sh
@@ -5,13 +5,7 @@ print_usage() {
 
 # Get directory of script
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
-SOURCE=${BASH_SOURCE[0]}
-while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-  SOURCE=$(readlink "$SOURCE")
-  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 WITH_TORCH=0
 while getopts 'd:ictpg' flag; do
