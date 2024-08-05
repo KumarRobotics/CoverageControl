3a080cd26545b2ca4bfac0ec6bd5119ff22c149f
Saurav Agarwal <agr.saurav1@gmail.com>
2024-08-05 03:53:05 -0400

    update to accept parameter file

--- python/scripts/coverage_env/world_idf.py
+++ python/scripts/coverage_env/world_idf.py
@@ -2,15 +2,22 @@
 A simple example of using WorldIDF
 """
 
+import sys
 import coverage_control
 
-params = coverage_control.Parameters()
-params.pNumRobots = 1
-params.pNumFeatures = 5
-params.pNumPolygons = 20
-params.pWorldMapSize = 1024
-params.pMaxVertices = 5
-params.pPolygonRadius = 128
+if len(sys.argv) > 1:
+    params_file = sys.argv[1]
+    params = coverage_control.Parameters(params_file)
+    params.pPlotScale = 2
+    params.pTruncationBND = 2
+else:
+    params = coverage_control.Parameters()
+    params.pNumRobots = 1
+    params.pNumFeatures = 5
+    params.pNumPolygons = 20
+    params.pWorldMapSize = 1024
+    params.pMaxVertices = 5
+    params.pPolygonRadius = 128
 
 env = coverage_control.CoverageSystem(params)
 
